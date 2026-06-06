62fad6e296b7c2f2a3e143022973acb13ca38e1c
unknown <hawasmuhaba4socials@gmail.com>
2026-06-06 01:05:25 -0700

    (feat): MiddleWare ordering impelemented

--- Program.cs
+++ Program.cs
@@ -1,17 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
+//services
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+//protected endpoint
+
+app.MapGet("/api/assessments/results", ()=>Results.Ok(new
+{
+    courseCode = "CS-101",
+    studentId= "S-001",
+    letterGrade = "A"
+})).RequireAuthorization();
 
 app.Run();
