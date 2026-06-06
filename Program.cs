56d431247cba10147754dda15bf394aec6c818e6
unknown <hawasmuhaba4socials@gmail.com>
2026-06-06 01:46:37 -0700

    (feat) custom request logging middleware implemented

--- Program.cs
+++ Program.cs
@@ -1,22 +1,31 @@
+using Microsoft.AspNetCore.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //services
-builder.Services.AddAuthentication();
+builder.Services
+.AddAuthentication("Training")
+.AddScheme<AuthenticationSchemeOptions,
+TrainingAuthHandler>("Training", null);
+
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+//middleware order
+
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-
-//protected endpoint
-
 app.MapGet("/api/assessments/results", ()=>Results.Ok(new
 {
     courseCode = "CS-101",
     studentId= "S-001",
     letterGrade = "A"
 })).RequireAuthorization();
+
+//protected endpoint
+
+
 
 app.Run();
