0ccf55553009ccfe02b124dc7be94250e67e03f3
Your GitHub Username <your.email@example.com>
2026-06-18 09:25:59 +0300

    obj mezez

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7180357743c82152839f463ca2df875b58c84b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c761cb896d95edfa4ea5b3ee78f63f3e0e608f19")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
